64a47941f389aa97b390c3937a6ad01abb421ff5
Marius Koudou <koudouxyz@gmail.com>
2021-08-05 14:35:47 +0000

    add cors

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 const config = {
+  allowOrigin: "*",
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
