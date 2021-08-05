6596f14db343a3596ebe50255a388d2ca7257137
Marius Koudou <koudouxyz@gmail.com>
2021-08-05 13:46:13 +0000

    add cors

--- index.js
+++ index.js
@@ -29,21 +29,11 @@ const config = {
 var allowedOrigins = ['http://localhost:4040'];
 
 const app = express();
-app.use(cors({
-  credentials: true,
-  origin: (origin, callback) => {
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
-app.options('*',cors())
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
@@ -56,20 +46,20 @@ if (!test) {
 }
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function (req, res) {
+app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 const port = process.env.PORT || 1337;
 if (!test) {
   const httpServer = require('http').createServer(app);
-  httpServer.listen(port, function () {
+  httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
   });
   // This will enable the Live Query real-time server
